13626fcf2a78bddb3c3957849c565ba39bc9bb01
Gary Mirams <gary.mirams@gmail.com>
2017-09-07 20:25:27 +0100

    Revert "Extra action potential error codes"
    This reverts commit 4c5ca636de8368844cac259464845978a8d94d6f.

--- src/single_cell/AbstractActionPotentialMethod.hpp
+++ src/single_cell/AbstractActionPotentialMethod.hpp
@@ -47,8 +47,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "SteadyStateRunner.hpp"
 
 /**
- * This is a class that provides methods for running cell models to steady state
- * and evaluating
+ * This is a class that provides methods for running cell models to steady state and evaluating
  * the resulting action potential markers like APD90.
  */
 class AbstractActionPotentialMethod
@@ -70,16 +69,10 @@ private:
     /** The error code (same as end of mErrorMessage) or 0 if no error occurred */
     unsigned mErrorCode;
 
-    /** Whether to say that no 1:1 correspondence between Stimuli and APs is an
-     * error, default is no, just warning */
+    /** Whether to say that no 1:1 correspondence between Stimuli and APs is an error, default is no, just warning */
     bool mNoOneToOneCorrespondenceIsError;
 
-    /** Whether to say that alternans behaviour is an error code. Default is no,
-     * just a warning. */
-    bool mAlternansIsError;
-
-    /** The threshold for an action potential (defaults to -50mV), altered if
-   * model is run at control conc of drug */
+    /** The threshold for an action potential (defaults to -50mV), altered if model is run at control conc of drug */
     double mActionPotentialThreshold;
 
     /** Whether the AP threshold was set manually (defaults to false) */
@@ -91,44 +84,42 @@ private:
     /**
      * A helper method, only available to this class.
      *
-     * @param pModel  A boost shared pointer to a cardiac cell model, with a
-     * RegularStimulus
+     * @param pModel  A boost shared pointer to a cardiac cell model, with a RegularStimulus
      * @param rApd90  double to populate with the postprocessed APD90 (ms)
      * @param rApd50  double to populate with the postprocessed APD50 (ms)
-     * @param rUpstroke  double to populate with the postprocessed maximum
-     * upstroke velocity (mV/ms)
+     * @param rUpstroke  double to populate with the postprocessed maximum upstroke velocity (mV/ms)
      * @param rPeak  double to populate with the postprocessed peak voltage (mV)
-     * @param rCaMax double to populate with the maximum cytosolic calcium
-     * concentration
-     * @param rCaMin double to populate with the minimum cytosolic calcium
-     * concentration
+     * @param rCaMax double to populate with the maximum cytosolic calcium concentration
+     * @param rCaMin double to populate with the minimum cytosolic calcium concentration
      * @param maximumTimeStep  The maximum CVODE time step to use (ms).
      * @param printingTimeStep  the printing time step to use (defaults to 1ms).
-     * @param conc  [optional] concentration argument (only used for more helpful
-     * warning messages).
+     * @param conc  [optional] concentration argument (only used for more helpful warning messages).
      *
      * @return the solution of the ODE.
      */
-    OdeSolution PerformAnalysisOfTwoPaces(
-        boost::shared_ptr<AbstractCvodeCell> pModel, double& rApd90,
-        double& rApd50, double& rUpstroke, double& rPeak, double& rCaMax,
-        double& rCaMin, const double s1_period, const double maximumTimeStep,
-        const double printingTimeStep, const double conc);
+    OdeSolution PerformAnalysisOfTwoPaces(boost::shared_ptr<AbstractCvodeCell> pModel,
+                                          double& rApd90,
+                                          double& rApd50,
+                                          double& rUpstroke,
+                                          double& rPeak,
+                                          double& rCaMax,
+                                          double& rCaMin,
+                                          const double s1_period,
+                                          const double maximumTimeStep,
+                                          const double printingTimeStep,
+                                          const double conc);
 
     /**
-     * A little method to 'push' cell model forward one S1 period, to get it 'in
-     * sync'
-     * if it is doing different alternans, or capturing the end of an AP in 2:1
-     * pacing:AP.
+     * A little method to 'push' cell model forward one S1 period, to get it 'in sync'
+     * if it is doing different alternans, or capturing the end of an AP in 2:1 pacing:AP.
      *
-     * @param pModel  A boost shared pointer to a cardiac cell model, with a
-     * RegularStimulus
+     * @param pModel  A boost shared pointer to a cardiac cell model, with a RegularStimulus
      * @param pacingCycleLength  The period of the regular stimulus in this model
      * @param maxTimeStep  The maximum CVODE time step to use in solving
      */
-    void PushModelForwardOneS1Interval(
-        boost::shared_ptr<AbstractCvodeCell> pModel, double pacingCycleLength,
-        double maxTimeStep);
+    void PushModelForwardOneS1Interval(boost::shared_ptr<AbstractCvodeCell> pModel,
+                                       double pacingCycleLength,
+                                       double maxTimeStep);
 
 protected:
     /** Whether to suppress output to std::cout */
@@ -140,183 +131,132 @@ protected:
     /** Whether the run was successful */
     bool mSuccessful;
 
-    /** Whether anything happened on a period-2 basis rather than period 1, e.g.
-   * alternans, long APs etc. */
+    /** Whether anything happened on a period-2 basis rather than period 1, e.g. alternans, long APs etc. */
     bool mPeriodTwoBehaviour;
 
     /**
-   * This method conducts a steady-state single cell simulation on a model
-   * (using its internal stimulus,
-   * which must be a RegularStimulus).
-   *
-   * It populates a number of steady state action potential property arguments
-   * and puts warnings to screen, or file
-   * if specified by subclasses.
-   *
-   * The mErrorMessage takes one of the following strings:
-   *  * NoActionPotential_1 - cell did not depolarise enough to form action
-   * potential
-   *  * NoActionPotential_2 - cell did not repolarise enough to form action
-   * potential
-   *  * NoActionPotential_3 - action potential did not repolarise in time for
-   * the next stimulus (no 1:1 correspondence) only an error if #SetLackOfOneToOneCorrespondenceIsError()
-   * has been called, otherwise it's a warning.
-   *  * NoActionPotential_4 - alternans or period two behaviour is occurring. This is only an error if
-   *  SetAlternansIsError() has been called, otherwise it is a warning.
-   *
-   * * NoActionPotential_5 - alternans or period two behaviour is occurring, but one AP is sub-threshold.
-   * This is only an error if #SetLackOfOneToOneCorrespondenceIsError() has been called,
-   * otherwise it is a warning.
-   *
-   * Optionally you can state a concentration (for more helpful warning
-   * messages) and printing time step.
-   *
-   * @param pModel  A boost shared pointer to a cardiac cell model, with a
-   * RegularStimulus
-   * @param rApd90  double to populate with the postprocessed APD90 (ms)
-   * @param rApd50  double to populate with the postprocessed APD50 (ms)
-   * @param rUpstroke  double to populate with the postprocessed maximum
-   * upstroke velocity (mV/ms)
-   * @param rPeak  double to populate with the postprocessed peak voltage (mV)
-   * @param rCaMax  double to populate with the maximum of calcium transient
-   * (mM)
-   * @param rCaMin  double to populate with the minimum of calcium transient
-   * (mM)
-   * @param printingTimeStep  the printing time step to use (defaults to 1ms).
-   * @param conc  [optional] concentration argument (only used for more helpful
-   * warning messages).
-   *
-   * @return the solution of the ODE.
-   */
-    OdeSolution SteadyStatePacingExperiment(
-        boost::shared_ptr<AbstractCvodeCell> pModel, double& rApd90,
-        double& rApd50, double& rUpstroke, double& rPeak, double& rCaMax,
-        double& rCaMin,
-        const double printingTimeStep = 1, // ms
-        const double conc = DOUBLE_UNSET);
+     * This method conducts a steady-state single cell simulation on a model (using its internal stimulus,
+     * which must be a RegularStimulus).
+     *
+     * It populates a number of steady state action potential property arguments and puts warnings to screen, or file
+     * if specified by subclasses.
+     *
+     * The mErrorMessage takes one of the following strings:
+     *  * NoActionPotential_1 - cell did not depolarise enough to form action potential
+     *  * NoActionPotential_2 - cell did not repolarise enough to form action potential
+     *  * NoActionPotential_3 - action potential did not repolarise in time for the next stimulus (no 1:1 correspondence).
+     * The last one is only an error if SetLackOfOneToOneCorrespondenceIsError() has been called, otherwise it's a warning.
+     *
+     * Optionally you can state a concentration (for more helpful warning messages) and printing time step.
+     *
+     * @param pModel  A boost shared pointer to a cardiac cell model, with a RegularStimulus
+     * @param rApd90  double to populate with the postprocessed APD90 (ms)
+     * @param rApd50  double to populate with the postprocessed APD50 (ms)
+     * @param rUpstroke  double to populate with the postprocessed maximum upstroke velocity (mV/ms)
+     * @param rPeak  double to populate with the postprocessed peak voltage (mV)
+     * @param rCaMax  double to populate with the maximum of calcium transient (mM)
+     * @param rCaMin  double to populate with the minimum of calcium transient (mM)
+     * @param printingTimeStep  the printing time step to use (defaults to 1ms).
+     * @param conc  [optional] concentration argument (only used for more helpful warning messages).
+     *
+     * @return the solution of the ODE.
+     */
+    OdeSolution SteadyStatePacingExperiment(boost::shared_ptr<AbstractCvodeCell> pModel,
+                                            double& rApd90,
+                                            double& rApd50,
+                                            double& rUpstroke,
+                                            double& rPeak,
+                                            double& rCaMax,
+                                            double& rCaMin,
+                                            const double printingTimeStep = 1, //ms
+                                            const double conc = DOUBLE_UNSET);
 
     /**
-   * This method just passes any message into the WARNINGS macro.
-   *
-   * It can be called from subclass methods to write a log message to the
-   * messages.txt file
-   * that should be displayed alongside the results.
-   * (for example a warning that the cell failed to de/re-polarise at a certain
-   * concentration.)
-   * @param rMessage  The message to write to file
-   */
+     * This method just passes any message into the WARNINGS macro.
+     *
+     * It can be called from subclass methods to write a log message to the messages.txt file
+     * that should be displayed alongside the results.
+     * (for example a warning that the cell failed to de/re-polarise at a certain concentration.)
+     * @param rMessage  The message to write to file
+     */
     virtual void WriteMessageToFile(const std::string& rMessage);
 
 public:
     /**
-   * The main constructor, sets a few defaults and reads some command line
-   * options.
-   */
+     * The main constructor, sets a few defaults and reads some command line options.
+     */
     AbstractActionPotentialMethod();
 
     /**
-   * Destructor
-   */
+     * Destructor
+     */
     virtual ~AbstractActionPotentialMethod(){};
 
     /**
-   * Set the maximum number of paces to run for.
-   * Use of this method will override any command line modifications
-   * set in the constructor.
-   *
-   * @param numPaces  The maximum number of paces to run in an attempt to get to
-   * steady state.
-   */
+     * Set the maximum number of paces to run for.
+     * Use of this method will override any command line modifications
+     * set in the constructor.
+     *
+     * @param numPaces  The maximum number of paces to run in an attempt to get to steady state.
+     */
     void SetMaxNumPaces(unsigned numPaces);
 
     /**
-   * @return  whether an error occurred in the action potential marker
-   * evaluation (not simulation itself).
-   */
+     * @return  whether an error occurred in the action potential marker evaluation (not simulation itself).
+     */
     bool DidErrorOccur(void);
 
     /**
-   * @return The error message from attempting to evaluate markers, such as
-   * APD90.
-   *
-   * This is "NoActionPotential_1" if the cell failed to depolarise (no AP
-   * occurs).
-   *
-   * Or "NoActionPotential_2" if the cell failed to repolarise (AP never returns
-   * to resting potential).
-   *
-   * Or "NoActionPotential_3" action potential did not repolarise in time for
-   * the next stimulus (no 1:1 correspondence).
-   *         This only occurs if #SetLackOfOneToOneCorrespondenceIsError() has
-   * been called.
-   *
-   * Or "NoActionPotential_4" if alternans is occuring (different APD on subsequent beats).
-   * This only occurs if #SetAlternansIsError() has been called,
-   * otherwise a warning is printed to screen.
-   *
-   * Or "NoActionPotential_5" if alternans is occuring but second AP is sub-threshold on
-   * the next stimulus (no 1:1 correspondence).
-   * This only occurs if #SetLackOfOneToOneCorrespondenceIsError() has been called.
-   */
+     * @return The error message from attempting to evaluate markers, such as APD90.
+     *
+     * This is "NoActionPotential_1" if the cell failed to depolarise (no AP occurs).
+     *
+     * Or "NoActionPotential_2" if the cell failed to repolarise (AP never returns to resting potential).
+     *
+     * Or "NoActionPotential_3" action potential did not repolarise in time for the next stimulus (no 1:1 correspondence).
+     *         This only occurs if #SetLackOfOneToOneCorrespondenceIsError() has been called.
+     */
     std::string GetErrorMessage();
 
     /**
-   * @return The error code from attempting to evaluate markers, such as APD90.
-   *
-   * Returns "0" if no error occurred.
-   *
-   * This is "1" if the cell failed to depolarise (no AP occurs).
-   *
-   * Or "2" if the cell failed to repolarise (AP never returns to resting
-   * potential).
-   *
-   * Or "3" action potential did not repolarise in time for the next stimulus
-   * (no 1:1 correspondence).
-   *         This only occurs if #SetLackOfOneToOneCorrespondenceIsError() has
-   * been called.
-   *
-   * Or "4" if alternans is occuring (different APD on subsequent beats).
-   * This only occurs if #SetAlternansIsError() has been called,
-   * otherwise a warning is printed to screen.
-   *
-   * Or "5" if alternans is occuring but second AP is sub-threshold on
-   * the next stimulus (no 1:1 correspondence).
-   * This only occurs if #SetLackOfOneToOneCorrespondenceIsError() has been called.
-   */
+     * @return The error code from attempting to evaluate markers, such as APD90.
+     *
+     * Returns "0" if no error occurred.
+     *
+     * This is "1" if the cell failed to depolarise (no AP occurs).
+     *
+     * Or "2" if the cell failed to repolarise (AP never returns to resting potential).
+     *
+     * Or "3" action potential did not repolarise in time for the next stimulus (no 1:1 correspondence).
+     *         This only occurs if #SetLackOfOneToOneCorrespondenceIsError() has been called.
+     */
     unsigned GetErrorCode();
 
     /**
-   * Set the voltage which is considered to be a triggered action potential.
-   *
-   * If this is not set it defaults to -50mV.
-   *
-   * @param threshold
-   */
+     * Set the voltage which is considered to be a triggered action potential.
+     *
+     * If this is not set it defaults to -50mV.
+     *
+     * @param threshold
+     */
     void SetVoltageThresholdForRecordingAsActionPotential(double threshold);
 
     /**
-   * Tell the simulator whether to suppress output to std::cout.
-   *
-   * @param suppress  Whether to suppress output (defaults to true).
-   */
+     * Tell the simulator whether to suppress output to std::cout.
+     *
+     * @param suppress  Whether to suppress output (defaults to true).
+     */
     void SuppressOutput(bool suppress = true);
 
     /**
-   * Tell this class to treat a lack of 1:1 correspondence between stimuli and
-   * action potentials
-   * as an error, rather than just a warning.
-   */
+     * Tell this class to treat a lack of 1:1 correspondence between stimuli and action potentials
+     * as an error, rather than just a warning.
+     */
     void SetLackOfOneToOneCorrespondenceIsError(bool errorOn = true);
 
     /**
-   * Tell this class to treat alternans / period two behaviour as an error,
-   * rather than just a warning.
-   */
-    void SetAlternansIsError(bool errorOn = true);
-
-    /**
-   * Reset the action potential evaluator for a new run.
-   */
+     * Reset the action potential evaluator for a new run.
+     */
     void Reset();
 };
 
