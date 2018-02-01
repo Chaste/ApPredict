b141151f373c31a5d09371ea4a59900974dd5319
Gary Mirams <gary.mirams@gmail.com>
2018-02-01 11:59:41 +0000

    Fix one nightly test problem

--- test/TestApPredictLong.hpp
+++ test/TestApPredictLong.hpp
@@ -40,27 +40,26 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <cxxtest/TestSuite.h>
 
 #include <boost/shared_ptr.hpp>
-#include "CommandLineArgumentsMocker.hpp"
 #include "ApPredictMethods.hpp"
+#include "CommandLineArgumentsMocker.hpp"
 
 class TestApPredictLong : public CxxTest::TestSuite
 {
 public:
-
     /**
      * This test will wipe $CHASTE_TEST_OUTPUT/ApPredict_output/
      *
      * The tests overwrite CommandLineArguments and does some standard
      * simulations to check things are working OK...
      */
-    void TestSomeFavouriteCompounds(void) throw (Exception)
+    void TestSomeFavouriteCompounds(void) throw(Exception)
     {
         // Test a simple hERG block with TT06
         // loop over hardcoded and dynamically loaded.
-        for (unsigned i=0; i<2; i++)
+        for (unsigned i = 0; i < 2; i++)
         {
             std::string model_option;
-            if (i==0)
+            if (i == 0)
             {
                 model_option = "--model 2";
             }
@@ -74,19 +73,19 @@ public:
             methods.Run();
             std::vector<double> concs = methods.GetConcentrations();
 
-            TS_ASSERT_EQUALS(concs.size(),3u);
-            TS_ASSERT_DELTA(concs[0], 0.0,   1e-12);
-            TS_ASSERT_DELTA(concs[1], 1.0,   1e-12);
-            TS_ASSERT_DELTA(concs[2], 10.0,  1e-12);
+            TS_ASSERT_EQUALS(concs.size(), 3u);
+            TS_ASSERT_DELTA(concs[0], 0.0, 1e-12);
+            TS_ASSERT_DELTA(concs[1], 1.0, 1e-12);
+            TS_ASSERT_DELTA(concs[2], 10.0, 1e-12);
 
             std::vector<double> apd90s = methods.GetApd90s();
-            TS_ASSERT_EQUALS(apd90s.size(),3u);
+            TS_ASSERT_EQUALS(apd90s.size(), 3u);
             TS_ASSERT_DELTA(apd90s[0], 301.4617, 1e-3);
             TS_ASSERT_DELTA(apd90s[1], 302.6703, 1e-3);
             TS_ASSERT_DELTA(apd90s[2], 311.27, 1e-2);
 
             TS_ASSERT_THROWS_THIS(methods.GetApd90CredibleRegions(),
-                "There was no Lookup Table available for credible interval calculations with these settings.");
+                                  "There was no Lookup Table available for credible interval calculations with these settings.");
         }
     }
 
@@ -97,14 +96,14 @@ public:
             CommandLineArgumentsMocker wrapper("--model 1 --plasma-concs 1 10 --pic50-ik1 4.5 --credible-intervals --plasma-conc-logscale false");
             ApPredictMethods methods;
             TS_ASSERT_THROWS_THIS(methods.Run(),
-                "Lookup table (for --credible-intervals) is currently only including IKr, IKs, INa and ICaL block, you have specified additional ones so quitting.");
+                                  "Lookup table (for --credible-intervals) is currently only including up to IKr, IKs, INa and ICaL block, you have specified additional ones so quitting.");
         }
 
         {
             CommandLineArgumentsMocker wrapper("--model 1 --plasma-concs 1 10 --pic50-herg 4.5 --credible-intervals --plasma-conc-logscale false");
             ApPredictMethods methods;
             TS_ASSERT_THROWS_THIS(methods.Run(),
-                "No argument --pic50-spread-herg has been provided. Cannot calculate credible intervals without this.");
+                                  "No argument --pic50-spread-herg has been provided. Cannot calculate credible intervals without this.");
         }
 
         // Test a simple hERG block with Shannon
@@ -115,19 +114,19 @@ public:
             methods.Run();
             std::vector<double> concs = methods.GetConcentrations();
 
-            TS_ASSERT_EQUALS(concs.size(),3u);
-            TS_ASSERT_DELTA(concs[0], 0.0,   1e-12);
-            TS_ASSERT_DELTA(concs[1], 1.0,   1e-12);
-            TS_ASSERT_DELTA(concs[2], 10.0,  1e-12);
+            TS_ASSERT_EQUALS(concs.size(), 3u);
+            TS_ASSERT_DELTA(concs[0], 0.0, 1e-12);
+            TS_ASSERT_DELTA(concs[1], 1.0, 1e-12);
+            TS_ASSERT_DELTA(concs[2], 10.0, 1e-12);
 
             std::vector<double> apd90s = methods.GetApd90s();
-            TS_ASSERT_EQUALS(apd90s.size(),3u);
+            TS_ASSERT_EQUALS(apd90s.size(), 3u);
             TS_ASSERT_DELTA(apd90s[0], 211.9333, 1e-3);
             TS_ASSERT_DELTA(apd90s[1], 213.9762, 1e-3);
             TS_ASSERT_DELTA(apd90s[2], 229.7436, 1e-3);
 
-            std::vector<std::pair<double,double> > apd90_credible_regions = methods.GetApd90CredibleRegions();
-            TS_ASSERT_EQUALS(apd90_credible_regions.size(),3u);
+            std::vector<std::pair<double, double> > apd90_credible_regions = methods.GetApd90CredibleRegions();
+            TS_ASSERT_EQUALS(apd90_credible_regions.size(), 3u);
             TS_ASSERT_DELTA(apd90_credible_regions[0].first, 211.9333, 1e-3);
             TS_ASSERT_DELTA(apd90_credible_regions[0].second, 211.9333, 1e-3);
             TS_ASSERT_DELTA(apd90_credible_regions[1].first, 212.5283, 1e-3);
@@ -144,19 +143,19 @@ public:
             methods.Run();
             std::vector<double> concs = methods.GetConcentrations();
 
-            TS_ASSERT_EQUALS(concs.size(),3u);
-            TS_ASSERT_DELTA(concs[0], 0.0,   1e-12);
-            TS_ASSERT_DELTA(concs[1], 1.0,   1e-12);
-            TS_ASSERT_DELTA(concs[2], 10.0,  1e-12);
+            TS_ASSERT_EQUALS(concs.size(), 3u);
+            TS_ASSERT_DELTA(concs[0], 0.0, 1e-12);
+            TS_ASSERT_DELTA(concs[1], 1.0, 1e-12);
+            TS_ASSERT_DELTA(concs[2], 10.0, 1e-12);
 
             std::vector<double> apd90s = methods.GetApd90s();
-            TS_ASSERT_EQUALS(apd90s.size(),3u);
+            TS_ASSERT_EQUALS(apd90s.size(), 3u);
             TS_ASSERT_DELTA(apd90s[0], 211.9333, 1e-3);
             TS_ASSERT_DELTA(apd90s[1], 213.9730, 1e-3);
             TS_ASSERT_DELTA(apd90s[2], 229.7191, 1e-3);
 
-            std::vector<std::pair<double,double> > apd90_credible_regions = methods.GetApd90CredibleRegions();
-            TS_ASSERT_EQUALS(apd90_credible_regions.size(),3u);
+            std::vector<std::pair<double, double> > apd90_credible_regions = methods.GetApd90CredibleRegions();
+            TS_ASSERT_EQUALS(apd90_credible_regions.size(), 3u);
             TS_ASSERT_DELTA(apd90_credible_regions[0].first, 211.9333, 1e-3);
             TS_ASSERT_DELTA(apd90_credible_regions[0].second, 211.9333, 1e-3);
             TS_ASSERT_DELTA(apd90_credible_regions[1].first, 213.1849, 1e-3);
@@ -171,9 +170,9 @@ public:
         // (which is what we are doing now).
         {
             CommandLineArgumentsMocker wrapper("--model 1 --pacing-freq 0.5 "
-                    "--pic50-herg 4.86 4.45 0 4.1429 --hill-herg 3.22 1.57 1 1 "
-                    "--pic50-spread-herg 0.15 --hill-spread-herg 0.21 "
-                    "--credible-intervals --plasma-concs 30.0");
+                                               "--pic50-herg 4.86 4.45 0 4.1429 --hill-herg 3.22 1.57 1 1 "
+                                               "--pic50-spread-herg 0.15 --hill-spread-herg 0.21 "
+                                               "--credible-intervals --plasma-concs 30.0");
 
             ApPredictMethods methods;
             methods.Run();
@@ -190,8 +189,8 @@ public:
         // different hERG IC50 of 50.5301 uM
         {
             CommandLineArgumentsMocker wrapper("--model 1 --pacing-freq 0.5 "
-                    "--pic50-herg 4.86 4.45 0 4.1429 --hill-herg 3.22 1.57 1 1 "
-                    "--plasma-concs 30.0 ");
+                                               "--pic50-herg 4.86 4.45 0 4.1429 --hill-herg 3.22 1.57 1 1 "
+                                               "--plasma-concs 30.0 ");
 
             ApPredictMethods methods;
             methods.Run();
@@ -207,8 +206,8 @@ public:
         // antagonist (inhibitor) of a channel.
         {
             CommandLineArgumentsMocker wrapper("--model 1 --pacing-freq 0.5 "
-                    "--pic50-herg 4.86 4.45 0 4.1429 --hill-herg 3.22 1.57 1 1 "
-                    "--plasma-concs 30.0 --saturation-herg 110 120 130.2 105.7");
+                                               "--pic50-herg 4.86 4.45 0 4.1429 --hill-herg 3.22 1.57 1 1 "
+                                               "--plasma-concs 30.0 --saturation-herg 110 120 130.2 105.7");
 
             ApPredictMethods methods;
             methods.Run();
@@ -221,7 +220,7 @@ public:
         }
     }
 
-    void TestWithAModelInAlternans(void) throw (Exception)
+    void TestWithAModelInAlternans(void) throw(Exception)
     {
         // We are trying to go so fast we get alternans, and then handle it nicely.
         CommandLineArgumentsMocker wrapper("--model 3 --plasma-concs 1 10 --pacing-freq 5 --plasma-conc-logscale false");
@@ -230,19 +229,19 @@ public:
         methods.Run();
         std::vector<double> concs = methods.GetConcentrations();
 
-        TS_ASSERT_EQUALS(concs.size(),3u);
-        TS_ASSERT_DELTA(concs[0], 0.0,   1e-12);
-        TS_ASSERT_DELTA(concs[1], 1.0,   1e-12);
-        TS_ASSERT_DELTA(concs[2], 10.0,  1e-12);
+        TS_ASSERT_EQUALS(concs.size(), 3u);
+        TS_ASSERT_DELTA(concs[0], 0.0, 1e-12);
+        TS_ASSERT_DELTA(concs[1], 1.0, 1e-12);
+        TS_ASSERT_DELTA(concs[2], 10.0, 1e-12);
 
         std::vector<double> apd90s = methods.GetApd90s();
-        TS_ASSERT_EQUALS(apd90s.size(),3u);
+        TS_ASSERT_EQUALS(apd90s.size(), 3u);
         TS_ASSERT_DELTA(apd90s[0], 137.3714, 1e-2);
         TS_ASSERT_DELTA(apd90s[1], 137.3714, 1e-2);
         TS_ASSERT_DELTA(apd90s[2], 137.3714, 1e-2);
     }
 
-    void TestWithAModelTwoToOneStimAp(void) throw (Exception)
+    void TestWithAModelTwoToOneStimAp(void) throw(Exception)
     {
         // We should go 'too fast' for O'Hara and see if we can get anything like sensible output.
         CommandLineArgumentsMocker wrapper("--model 6 --plasma-concs 1 --pacing-freq 5 --plasma-conc-logscale false");
@@ -251,42 +250,39 @@ public:
         methods.Run();
         std::vector<double> concs = methods.GetConcentrations();
 
-        TS_ASSERT_EQUALS(concs.size(),2u);
-        TS_ASSERT_DELTA(concs[0], 0.0,   1e-12);
-        TS_ASSERT_DELTA(concs[1], 1.0,   1e-12);
+        TS_ASSERT_EQUALS(concs.size(), 2u);
+        TS_ASSERT_DELTA(concs[0], 0.0, 1e-12);
+        TS_ASSERT_DELTA(concs[1], 1.0, 1e-12);
 
         std::vector<double> apd90s = methods.GetApd90s();
-        TS_ASSERT_EQUALS(apd90s.size(),2u);
+        TS_ASSERT_EQUALS(apd90s.size(), 2u);
         TS_ASSERT_DELTA(apd90s[0], 266.81, 1e-1); // Very sensitive to compiler changes, so high tolerance.
         TS_ASSERT_DELTA(apd90s[1], 266.81, 1e-1); // Very sensitive to compiler changes, so high tolerance.
     }
 
-    void TestTroublesomeApCalculation(void) throw (Exception)
+    void TestTroublesomeApCalculation(void) throw(Exception)
     {
         // This is a case that Gef was having some trouble with. It does look as if something odd is going
         // on, as it reports AP alternans, then tries to swap round the pacing and it has gone, at 10k paces.
         CommandLineArgumentsMocker wrapper("--model 1 --pacing-freq 0.5 --pic50-cal 3.0 --hill-cal 1 "
-                "--pic50-herg 0 --hill-herg 1 "
-                "--pic50-na 4.561 --hill-na 1 "
-                "--plasma-concs 0 100.0");
+                                           "--pic50-herg 0 --hill-herg 1 "
+                                           "--pic50-na 4.561 --hill-na 1 "
+                                           "--plasma-concs 0 100.0");
 
         ApPredictMethods methods;
         methods.Run();
         std::vector<double> concs = methods.GetConcentrations();
 
-        TS_ASSERT_EQUALS(concs.size(),3u);
+        TS_ASSERT_EQUALS(concs.size(), 3u);
 
         std::vector<double> apd90s = methods.GetApd90s();
-        TS_ASSERT_EQUALS(apd90s.size(),3u);
+        TS_ASSERT_EQUALS(apd90s.size(), 3u);
         TS_ASSERT_DELTA(apd90s[0], 219.60, 1e-1); // Very sensitive to compiler changes, so high tolerance.
         TS_ASSERT_DELTA(apd90s[1], 219.60, 1e-1);
         TS_ASSERT_DELTA(apd90s[2], 217.469, 1e-1);
     }
-
 };
-
 
 #endif //_TESTAPPREDICTLONG_HPP_
 
 #endif //_CHASTE_CVODE
-
