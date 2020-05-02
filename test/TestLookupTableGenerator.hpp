3b02e2f9fb5eef80305760375ffb447d4f012ce5
Gary Mirams <gary.mirams@gmail.com>
2020-05-02 14:28:45 +0100

    Added a threshold test for model 8

--- test/TestLookupTableGenerator.hpp
+++ test/TestLookupTableGenerator.hpp
@@ -131,7 +131,7 @@ public:
     void TestVoltageThresholdDetectionAlgorithm()
     {
         std::vector<double> thresholds_for_each_model = boost::assign::list_of(-46.6219) /*Shannon etc.*/
-            (-44.3656)(-34.5945)(-35.9230)(-28.3257)(-38.4384)(0.0);
+            (-44.3656)(-34.5945)(-35.9230)(-28.3257)(-38.4384)(-40.3365);
 
         for (unsigned model_index = 1; model_index < 8u; model_index++)
         {
